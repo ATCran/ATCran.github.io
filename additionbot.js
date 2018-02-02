0a5607c89f1735eead1765b12b33d1d44a6ea2e1
ATCran <35800168+ATCran@users.noreply.github.com>
2018-02-01 22:13:42 -0500

    Script file
    AdditionBot script file to form page.  Title changes on both pages.  Removing line breaks.

---
+++ additionbot.js
@@ -0,0 +1,4 @@
+var first = prompt("I am AdditionBot.  Give me a number.");
+var second = prompt("Give me another number and be amazed.");
+var sum = Number(first) + Number(second);
+alert("Easy.  The sum is " + sum + ".");
